13096caa406660fa498420fc5a543ecbd4d30558
matt2ology <34324060+matt2ology@users.noreply.github.com>
2023-11-25 17:14:01 -0800

    Attempt to make logic more clear

--- 06-ajax/encrypt-it.js
+++ 06-ajax/encrypt-it.js
@@ -64,9 +64,9 @@
     for (let i = 0; i < text.length; i++) {
       let charCode = text.charCodeAt(i);
       if (charCode >= 65 && charCode <= 90) {
-        result += String.fromCharCode((charCode - 65 + 1) % 26 + 65);
+        result += String.fromCharCode(((charCode - 65 + 1) % 26) + 65);
       } else if (charCode >= 97 && charCode <= 122) {
-        result += String.fromCharCode((charCode - 97 + 1) % 26 + 97);
+        result += String.fromCharCode(((charCode - 97 + 1) % 26) + 97);
       } else {
         result += text.charAt(i);
       }
